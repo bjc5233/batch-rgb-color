9bc8a4f6c439a8141a0bf0bbf3b0a2c04fb55b8a
bjc5233 <bjc5233@gmail.com>
2018-01-15 13:34:26 +0800

    change desc

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 # batch-rgb-color
 
 ## 说明
-> 设置终端RGB颜色（原生只能使用系统定义的16种颜色）
+> 动态修改当前字体颜色槽为指定的RGB颜色
 
 
 ## 调用参数
 * R G B [-d default]
-* R - 红色代码[0-255]
-* G - 绿色代码[0-255]
-* B - 蓝色代码[0-255]
+* R - 红色代码[0-255], 无则自动生成随机值
+* G - 绿色代码[0-255], 无则自动生成随机值
+* B - 蓝色代码[0-255], 无则自动生成随机值
 * default - 字体颜色重置为原始颜色
 
 
+## 注意
+* 由于default参数是flag型参数，因此必须放到参数列表最后
+
 ## 预览
+<div align=center><img src="https://github.com/bjc5233/batch-rgb-color/raw/master/resources/colorTable.png"/></div>
 <div align=center><img src="https://github.com/bjc5233/batch-rgb-color/raw/master/resources/demo.png"/></div>
--- rgbColor.bat
+++ rgbColor.bat
@@ -1,12 +1,16 @@
 @echo off& call load.bat _getConsoleCurColor _getRandomNum _parseJSON& call loadF.bat _params _errorMsg _ifOr& call loadE.bat hex2dec& setlocal enabledelayedexpansion
 ::说明
-::  设置cmd字体颜色
+::  动态修改当前字体颜色槽为指定的RGB颜色
 ::参数
 ::  R G B [-d default]
-::  R - 红色代码[0-255]
-::  G - 绿色代码[0-255]
-::  B - 蓝色代码[0-255]
+::  R - 红色代码[0-255], 无则自动生成随机值
+::  G - 绿色代码[0-255], 无则自动生成随机值
+::  B - 蓝色代码[0-255], 无则自动生成随机值
 ::  default - 字体颜色重置为原始颜色
+::注意
+::  由于default参数是flag型参数，因此必须放到参数列表最后
+::TODO
+::  hex2dec命令调用略慢，考虑采用pure batch书写
 ::external
 ::  date       2018-01-15  2:18:28
 ::  face       555~
